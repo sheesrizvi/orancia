999a5563b5457b36a9b80c446d04dd4ad713ff2f
tanmay8598 <tanmay8598@gmail.com>
2024-05-24 12:36:07 +0530

    changes

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -1,8 +1,18 @@
 const express = require("express");
 const { admin } = require("../middleware/authmiddleware");
 
-const { deleteProduct, createProduct, getAllProduct, getProductById, getProductByGroupId, getProductInventory } = require("../controller/productController");
-const { createInput, getAllInput } = require("../controller/inventoryController");
+const {
+  deleteProduct,
+  createProduct,
+  getAllProduct,
+  getProductById,
+  getProductByGroupId,
+  getProductInventory,
+} = require("../controller/productController");
+const {
+  createInput,
+  getAllInput,
+} = require("../controller/inventoryController");
 const router = express.Router();
 
 router.route("/create").post(createProduct);
@@ -13,9 +23,7 @@ router.route("/inventory").get(getProductInventory);
 router.route("/create-input").post(createInput);
 router.route("/getall-input").get(getAllInput);
 
-
 // delete
 router.route("/delete").delete(deleteProduct);
-
 
 module.exports = router;
