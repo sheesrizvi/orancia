ce8e008e808c03639131dee4a17a8ff63e21b9d4
tanmay8598 <tanmay8598@gmail.com>
2024-05-25 17:35:17 +0530

    product routes

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -24,6 +24,8 @@ router.route("/get-by-groupid").get(getProductByGroupId);
 router.route("/inventory").get(getProductInventory);
 router.route("/create-input").post(createInput);
 router.route("/getall-input").get(getAllInput);
+router.route("/create-review").post(createProductReview);
+router.route("/search-product").get(searchProducts);
 
 // delete
 router.route("/delete").delete(deleteProduct);
